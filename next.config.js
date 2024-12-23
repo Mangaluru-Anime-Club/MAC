f7690cd828475b09a2b79e25496626121596e800
GGAshwin <ashwinprabhu2001@gmail.com>
2024-12-23 10:02:17 +0530

    chaged next config for github pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/MAC", // Replace with your repository name
+  assetPrefix: "/MAC", // Replace with your repository name
+  images: {
+    unoptimized: true, // Required for GitHub Pages since server-side optimization is unavailable
+  },
+};
 
 module.exports = nextConfig;
